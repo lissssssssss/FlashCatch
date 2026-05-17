978f7074652656efb9e2b88c0fe9120c2b2f78bc
lishengsheng <lishengsheng2020@outlook.com>
2026-05-17 20:58:23 +0800

    Initial Commit

---
+++ FlashCatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlashCatch
+//
+//  Created by lishengsheng on 2026/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlashCatch/FlashCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCatchApp.swift
+//  FlashCatch
+//
+//  Created by lishengsheng on 2026/5/17.
+//
+
+import SwiftUI
+
+@main
+struct FlashCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
